5f188f3439ea9a5d5a8a4da83957cc8ccaa06965
Shun Yao <ysmiracle@hotmail.com>
2018-04-23 03:31:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Shun"
+  end
+
+  get '/hometown' do
+    "My hometown is Whitestone, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Madness - Muse"
+  end
 end
